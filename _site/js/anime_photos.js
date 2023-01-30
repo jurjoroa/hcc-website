109fe8b0592c0f02d31bdaf47e49496775a68cc3
Jorge Roa <64996320+jurjoroa@users.noreply.github.com>
2023-01-30 12:56:48 +0100

    Update scroll

--- _site/js/anime_photos.js
+++ _site/js/anime_photos.js
@@ -39,3 +39,19 @@ function isElementInViewport(el) {
   );
 }
 
+
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+}
+
+
+
+
